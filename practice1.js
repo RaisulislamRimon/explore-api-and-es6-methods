3e64fc88c6f43f6779d58677c63aaa5c67172710
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-30 16:08:58 +0000

    open-library-project-api

---
+++ practice1.js
@@ -0,0 +1,12 @@
+const personJson = {
+  name: "John",
+  age: 30,
+  isMarried: false,
+};
+
+console.log(personJson);
+console.log(personJson.name);
+console.log(personJson);
+console.log(typeof personJson);
+console.log(JSON.stringify(personJson));
+console.log(typeof personJson);
