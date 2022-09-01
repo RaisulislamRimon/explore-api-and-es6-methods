9e3ab5b4cbd0d5a9377f069141e4b9761d1ba6cc
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-01 09:53:58 +0000

    update

--- library.js
+++ library.js
@@ -9,6 +9,29 @@ const searchBook = () => {
     });
 };
 
+const resultContainer = document.getElementById("search-result");
 const displaySearchResult = (book) => {
-  console.log(book);
+  // console.log(book);
+  book.forEach((singleBook) => {
+    const {
+      title,
+      edition_count,
+      publish_date,
+      number_of_pages_median,
+      author_name,
+    } = singleBook;
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("col");
+    bookCard.innerHTML = `
+      <div class="card shadow-lg">
+        <div class="card-body">
+          <h5 class="card-title">Book Title: ${title}</h5>
+          <p class="card-text">Edition Count: ${edition_count[0]}</p>
+          <p class="card-text">Publish Date: ${publish_date}</p>
+          <p class="card-text">Number of Pages: ${number_of_pages_median}</p>
+          <p class="card-text">Author: ${author_name}</p>
+      </div>
+    `;
+    resultContainer.appendChild(bookCard);
+  });
 };
